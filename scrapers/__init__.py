d92d9cef3e2dcfcfcaee34fc4aa82c3447dc35de
samgzzgzz2-max <samgzzgzz2@gmail.com>
2026-08-03 14:24:42 -0600

    Add scrapers/__init__.py

---
+++ scrapers/__init__.py
@@ -0,0 +1,13 @@
+from .mercadolibre import MercadoLibreScraper
+from .chedraui import ChedrauiScraper
+from .soriana import SorianaScraper
+from .heb import HEBScraper
+from .walmart import WalmartScraper
+
+__all__ = [
+    "MercadoLibreScraper",
+    "ChedrauiScraper",
+    "SorianaScraper",
+    "HEBScraper",
+    "WalmartScraper",
+]
